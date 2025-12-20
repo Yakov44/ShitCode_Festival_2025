acfb10b0320a74ceaea75cd5282f900eb028d3e8
yakov <mr.afonichkin2018@gmail.com>
2025-12-20 17:47:49 +0300

    Вывести Hello World! в консоль

---
+++ Hello world!.py
@@ -0,0 +1,39 @@
+from string import punctuation as r13slS33RFEWFE9SDIJOIQW3F
+from string import ascii_uppercase as r13sIS33RFEWFE9SDIJOIQW3F
+from random import sample as r13SLS33PREWFE0SDLJOIQW3F
+from random import choice as r13SLS33PREWFE0SDLJQIQW3F
+
+r13SLS33PREWFE0SDLJQIQV3F = ['o', 'e', 'l', 'l']
+r13SLS33PREWFE0SDIJQIQV3F = ['r', 'l', 'd', 'w', 'o', ' ']
+r13SLS33PREWFE0SDIJ0IQV3F = True
+while r13SLS33PREWFE0SDIJ0IQV3F:
+    r13sIS33RFEWPE9SDIJOIQW3F = ''.join(r13SLS33PREWFE0SDLJQIQW3F(r13sIS33RFEWFE9SDIJOIQW3F))
+    if r13sIS33RFEWPE9SDIJOIQW3F == 'H':
+        r13SLS33PREWFE0CDLJQIQW3F = True
+        while r13SLS33PREWFE0CDLJQIQW3F:
+            r13SLS33PREWFE0SDIJ0I0V3F = ''.join(r13SLS33PREWFE0SDLJOIQW3F(r13SLS33PREWFE0SDLJQIQV3F, 4))
+            if r13SLS33PREWFE0SDIJ0I0V3F == 'ello':
+                r13sIS33RFEWPE9SBIJOIQW3F = True
+                while r13sIS33RFEWPE9SBIJOIQW3F:
+                    r13sIS33RFEWPE9SDLJOIQW3F = ''.join(r13SLS33PREWFE0SDLJQIQW3F(r13SLS33PREWFE0SDIJQIQV3F))
+                    if r13sIS33RFEWPE9SDLJOIQW3F == ' ':
+                        r13sIS33RFEVPE9SDIJOIQW3F = True
+                        while r13sIS33RFEVPE9SDIJOIQW3F:
+                            r13sIS33RFEVPE9SDIJQIQW3F = ''.join(r13SLS33PREWFE0SDLJQIQW3F(r13sIS33RFEWFE9SDIJOIQW3F))
+                            if r13sIS33RFEVPE9SDIJQIQW3F == 'W':
+                                r13SLS33PREWFE0SDIJQI0V3F = True
+                                while r13SLS33PREWFE0SDIJQI0V3F:
+                                    r13SLS33PREVFE0SDIJQI0W3F = ''.join(r13SLS33PREWFE0SDLJOIQW3F(r13SLS33PREWFE0SDIJQIQV3F, 4))
+                                    if r13SLS33PREVFE0SDIJQI0W3F == 'orld':
+                                        r13SLS33PREWFE0SDIJ0I0V3P = True
+                                        while r13SLS33PREWFE0SDIJ0I0V3P:
+                                            r13SLS33PREWFEQSDIJ0I0V3F = ''.join(r13SLS33PREWFE0SDLJQIQW3F(r13slS33RFEWFE9SDIJOIQW3F))
+                                            if r13SLS33PREWFEQSDIJ0I0V3F == '!':
+                                                r13SLS33PREWFE0SDIJ0I0V3P = False
+                                                r13SLS33PREWFE0SDIJQI0V3F = False
+                                                r13sIS33RFEWPE9SBIJOIQW3F = False
+                                                r13sIS33RFEVPE9SDIJOIQW3F = False
+                                                r13SLS33PREWFE0CDLJQIQW3F = False
+                                                r13SLS33PREWFE0SDIJ0IQV3F = False
+
+print(r13sIS33RFEWPE9SDIJOIQW3F + r13SLS33PREWFE0SDIJ0I0V3F + r13sIS33RFEWPE9SDLJOIQW3F + r13sIS33RFEVPE9SDIJQIQW3F + r13SLS33PREVFE0SDIJQI0W3F + r13SLS33PREWFEQSDIJ0I0V3F)
